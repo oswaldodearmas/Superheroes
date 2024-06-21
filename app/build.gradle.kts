4d35cb7c5eff3186f85a243375a07224b70c06a1
oswaldodearmas <oswaldodearmas@gmail.com>
2024-06-21 10:10:48 +0200

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.odearmas.superheroes"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
